f9dc8053bb65b9c558752cc8c53fdc3992c19956
Md Zama <83308074+MZKhan18@users.noreply.github.com>
2025-12-24 15:13:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,33 +74,6 @@ def extract_embedding_from_pil(pil_img):
     )[0]["embedding"]
 
     return np.array(embedding).reshape(1, -1)
-    
-ACTORS_ZIP = "Actors.zip"
-ACTORS_DIR = "Actors"
-ACTORS_URL = "https://drive.google.com/uc?id=1PNmNQ4cQ1_kn6p-9XXdVBZba_wRDVj77"
-
-@st.cache_resource
-def prepare_actor_images():
-    if not os.path.exists(ACTORS_DIR):
-        with st.spinner("Downloading reference face database..."):
-            gdown.download(ACTORS_URL, ACTORS_ZIP, quiet=False)
-
-        import zipfile
-        with zipfile.ZipFile(ACTORS_ZIP, "r") as zip_ref:
-            zip_ref.extractall()
-
-        os.remove(ACTORS_ZIP)
-
-prepare_actor_images()
-
-def load_actor_image(actor_dir):
-    try:
-        for file in os.listdir(actor_dir):
-            if file.lower().endswith((".jpg", ".jpeg", ".png")):
-                return Image.open(os.path.join(actor_dir, file))
-    except Exception:
-        pass
-    return None
 
 
 if uploaded_image is not None:
@@ -116,37 +89,26 @@ if uploaded_image is not None:
         query_embedding = extract_embedding_from_pil(display_img)
         best_index, best_score = find_best_match(query_embedding, feature_list)
 
-    matched_image_path = filenames[best_index]
+
+
+    matched_img = Image.open(filenames[best_index])
 
     with col2:
         st.subheader("Matched Identity")
 
-        matched_image_path = filenames[best_index]
+        matched_name = os.path.splitext(os.path.basename(filenames[best_index]))[0]
+        matched_name = re.split(r"[._]\d+$", matched_name)[0]
 
-        # Get actor directory and name
-        actor_dir = os.path.dirname(matched_image_path)
-        actor_name = os.path.basename(actor_dir)
-
-        st.markdown(f"###  {actor_name}")
-
-        if os.path.exists(actor_dir):
-            actor_img = load_actor_image(actor_dir)
-
-            if actor_img is not None:
-                st.image(actor_img, width=250)
-            else:
-                st.warning("⚠️ No image found in actor folder.")
-        else:
-            st.warning("⚠️ Actor folder not found.")
+        st.image(matched_img, width=250)
+        st.markdown(f"###  {matched_name}")
 
     st.markdown("---")
     st.subheader("🔎 Match Confidence")
 
     match_percent = int(best_score * 100)
+
     st.progress(match_percent)
     st.markdown(f"### {match_percent}% similarity")
-
-
 
 
 
